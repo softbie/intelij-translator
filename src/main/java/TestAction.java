fa1bf4b9926f498f31d3c7fe90cab6a5d685e0f9
13Hz <async13hhz@gmail.com>
2024-04-04 11:11:47 +0300

    Уведомления

--- src/main/java/TestAction.java
+++ src/main/java/TestAction.java
@@ -1,7 +1,10 @@
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
@@ -24,10 +27,10 @@ public class TestAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         Editor editor = anActionEvent.getData(PlatformDataKeys.EDITOR);
+        Project project = anActionEvent.getRequiredData(PlatformDataKeys.PROJECT);
         if (editor != null) {
             String selectedText = editor.getSelectionModel().getSelectedText();
             if (selectedText != null && !selectedText.isEmpty()) {
-//                Messages.showMessageDialog(editor.getSelectionModel().getSelectedText(), "Test", Messages.getInformationIcon());
                 String encodedText = URLEncoder.encode(selectedText, StandardCharsets.UTF_8);
                 try {
                     URL url = new URL(String.format("https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ru&hl=ru&dt=t&dt=bd&dj=1&source=icon&tk=810526.810526&q=%s", encodedText));
@@ -46,23 +49,32 @@ public class TestAction extends AnAction {
                         JSONObject json = new JSONObject(response.toString());
                         if (!json.getJSONArray("sentences").isEmpty()) {
                             JSONObject sentence = (JSONObject) json.getJSONArray("sentences").get(0);
-                            String translate = new String(sentence.getString("trans").getBytes(), StandardCharsets.UTF_8);
-
-                            Project project = anActionEvent.getRequiredData(PlatformDataKeys.PROJECT);
                             Document document = editor.getDocument();
                             Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
                             int start = primaryCaret.getSelectionStart();
                             int end = primaryCaret.getSelectionEnd();
                             WriteCommandAction.runWriteCommandAction(project, () ->
-                                    document.replaceString(start, end, translate)
+                                    document.replaceString(start, end, GetCyrillicText(sentence.getString("trans")))
                             );
                             primaryCaret.removeSelection();
+                            Notification notification = new Notification("translateSuccess", GetCyrillicText("Переведено"), GetCyrillicText("Строка успешно переведена"), NotificationType.INFORMATION);
+                            notification.notify(project);
+                        } else {
+                            Notification notification = new Notification("translateParseError", GetCyrillicText("Ошибка"), GetCyrillicText("Не удалось получить перевод"), NotificationType.ERROR);
+                            notification.notify(project);
                         }
+                    } else {
+                        Notification notification = new Notification("translate", GetCyrillicText("Ошибка"), GetCyrillicText("Пришел ответ, отличный от 200"), NotificationType.ERROR);
+                        notification.notify(project);
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    private String GetCyrillicText(String text) {
+        return new String(text.getBytes(), StandardCharsets.UTF_8);
     }
 }
